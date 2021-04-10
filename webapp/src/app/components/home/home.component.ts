3da9a8b9181284a9aa2a5dcaa10b03d3c97ce4e4
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-10 14:36:49 -0300

    MAP

--- webapp/src/app/components/home/home.component.ts
+++ webapp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const myMap = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
   }
-
 }
