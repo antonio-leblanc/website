192cccd0c04282ab9e1a52b58b12bd093c43fce6
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-10 14:24:23 -0300

    home works

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home', component: HomeComponent,
+    // children: [
+      // {path: 'map', component: MapComponent},
+      // {path: 'manager', component: ManagerComponent, data: {roles: [0, 20]}},
+      // {path: 'meteorologia', component: MeteoComponent},
+    // ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
