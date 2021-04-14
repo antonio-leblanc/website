7cfc6312c908aae849241ed123db26e494084b1d
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-13 23:41:31 -0300

    js review

--- code/main.js
+++ code/main.js
@@ -2,59 +2,77 @@
 
 //   <------------ On load Functions ------------->
 $(window).on("load",
-function(){ 
-  $(".loader-wrapper").fadeOut("slow");
+  function(){ 
+    $(".loader-wrapper").fadeOut("slow");
 
-  $(".typed-element").typed({
-      strings: ["ForestFire Engineering", "Cycling","Software Development",
-      "Piano","Data Science","Skateboarding", "Adventure"],
-      typeSpeed: 35,
-      backSpeed: 15,
-      loop:true,
-    });
+    $(".typed-element").typed(
+      {
+        strings: [
+          "ForestFire Engineering",
+          "Cycling",
+          "Software Development",
+          "Piano",
+          "Data Science",
+          "Skateboarding",
+          "Adventure"
+        ],
+        typeSpeed: 35,
+        backSpeed: 15,
+        loop:true,
+      }
+    );
 
-  $("#travel-map").css('visibility', 'visible')
+    $("#travel-map").css('visibility', 'visible')
 
-  }
+    }
 );
 
 //   <------------ Header Disappears ------------->
 
 if ( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    $("#header").css("top" , "0");
-  } else {
-    $("#header").css("top" , "-4rem");
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      $("#header").css("top" , "0");
+    } else {
+      $("#header").css("top" , "-4rem");
 
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
+
 }
 
 
 //   <------------ Slide do sections ------------->
 
-$('a[href^="#"]').click( function (e) {
+$('a[href^="#"]').click(
+  function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
-    });
+  }
+);
 
 
 //   <------------ Animations ------------->
+
 AOS.init({
-offset:150,
-duration:1000,
+  offset:150,
+  duration:1000,
 });
+
 //   <------------ navbarClick ------------->
+
 function menuClick(){
-    $('.navbar').toggleClass('active')
+  $('.navbar').toggleClass('active')
 }
+
+
 //   <------------ TabSelection ------------->
+
+
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
