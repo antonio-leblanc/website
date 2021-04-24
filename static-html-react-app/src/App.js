f527db91a30825397b3300833f218bc3390d7f31
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 16:05:25 -0300

    scss! #16

--- static-html-react-app/src/App.js
+++ static-html-react-app/src/App.js
@@ -2,7 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return null
+  return (
+      <div class="content-block">
+        <p>Hello there!</p>
+        <p>I'm Antonio, a fresh-graduate Mechanical Engineer, passionate about learning and acquiring new skills.</p>
+        <p>I want to make a positive impact on the world and fight climate change through forest fire detection.</p>
+        <p>In order to live a fulfilled life, I do my best to find balance beetwen these dimensions :</p>
+        <div class="styled-list">
+        <ul>
+          <li>Relationships</li>
+          <li>Self-improvement</li>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Work</li>
+        </ul>
+      </div>
+    </div>
+
+  )
 }
 
 export default App;
