09c6f38240b06d470725b15702401abeff5a0153
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-24 16:09:36 -0300

    #16 components

---
+++ static-html-react-app/src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div class="content-block">
+        <p>Hello there!</p>
+        <p>I'm Antonio, a fresh-graduate Mechanical Engineer, passionate about learning and acquiring new skills.</p>
+        <p>I want to make a positive impact on the world and fight climate change through forest fire detection.</p>
+        <p>In order to live a fulfilled life, I do my best to find balance beetwen these dimensions :</p>
+        <div class="styled-list">
+          <ul>
+            <li>Relationships</li>
+            <li>Self-improvement</li>
+            <li>Music</li>
+            <li>Sports</li>
+            <li>Work</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
